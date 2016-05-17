136b6f8e3773061b1a4ee5fd1223a5768b57b95c
ricardoparizotto <ricardo.dparizotto@gmail.com>
2016-05-17 17:27:45 -0300

    threads para os componentes

--- lib/serv_center.h
+++ lib/serv_center.h
@@ -5,27 +5,43 @@
  *
  */
 
-#define MAX_CENTERS 1000   //máximo de centros de serviço por componente
+#include <queue>   
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
 
-struct queues{
-    <queue>...//fila do c++
+#define MAX_S 1000 										//numero máximo de servidores
 
-    int entrada, saída;
-};
-
-struct representa_componentes{
-    /*vai possuir as filas e componentes | vai instanciar as classes
-     * vai relacionar entradas dos componentes as saídas das filas
-     * e saídas dos componentes para as entradas das filas
-     */
+struct agent{
+    int id, time;
 };
 
 
 struct component{
-    unsigned int servers, av_servers; // quantidade de servidores | quantidade de servidores disponiveis no momento. Se 0, pessoa aguarda na fila.
-    int min_time, max_time;   //atender entre 4 e 8 unidades de tempo
-    int 
+    unsigned int servers[MAX_S], numb_servers;               //indice é o servidor e o valor é o tempo do servidor
+    int min_time, max_time;  								 //Ex: atender entre min_time e max_time unidades de tempo
+    int in, out;											 //indíce das filas de entrada e saída
 
+
+    //retorna o id do servidor com menor tempo.
+    unsigned int mintime_server(){
+        int min = 0;
+        for ( int i = 1; i < this.numb_servers; i++ )
+            if(servers[i] < servers[min])
+                min = i;         
+        return min;    
+    }
+
+    //servidor "server" atende a pessoa "p"
+    void serving(int server, agent * p){
+        srand((unsigned)time(NULL));
+        int t = rand()%(this.min_time + 1);
+        this.servers[server]+=t;
+		//atualiza o tempo da pessoa. -- tempo da pessoa somado com o tempo de atendimento mais o tempo de espera na fila --
+        p->time+=t + (this.servers[server] - p->time);
+    }
 
 };
 
@@ -35,6 +51,47 @@ struct router{
 
     int jhonson_queue(int queue[]){}            //retorna uma das filas conforme a distribuição uniforme de probabilidade
 };
+
+/***************************************
+*filas de pessoas
+* mutexes para filas
+*******************************************/
+std::queue<agent> queues[;
+pthread_mutex_t * lockqueue;
+
+
+
+//função para as threads dos centros
+void * centro(void * v){
+	agent pers;  
+	component _center;
+	
+	//_center = (component*)v;
+
+	while(1){
+		if(queues[_center.in].empty())continue;					//se a fila estiver vazia, continua até que exista um elemento.
+        //seta semaforo para fila de entrada
+		pthread_mutex_lock(&lockqueue[_center.in]);
+        //remove pessoa da fila
+		pers = queues[_center.in].front();
+		queues[_center.in].pop();
+        //deseta semaforo para fila de entrada
+		pthread_mutex_unlock(&lockqueue[_center.in]);
+        //coloca o servidor "mintime_server" servir a pessoa
+		_center.serving(mintime_server(), &pers);
+        //seta semaforo para fila de saída
+		pthread_mutex_lock(&lockqueue[_center.out]);
+        //coloca a pessoa na fila de saída
+		queues[_center.out].push(pers);
+        //desbloqueia semaforo para fila de saída
+		pthread_mutex_unlock(&lockqueue[_center.out]);		
+    }
+}
+
+
+
+
+
 
 
 
