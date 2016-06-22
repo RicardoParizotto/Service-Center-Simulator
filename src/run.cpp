d17f2ed2d57a753143ed4213f78d069f13d796f3
Aluno (local) <ricardo.dparizotto@gmail.com>
2016-06-22 19:20:07 -0300

    run

--- src/run.cpp
+++ src/run.cpp
@@ -2,8 +2,13 @@
 #include "../lib/serv_center.hh"
 
 
-
 void run(){
-	
-
+	GVT = 0;
+	while( GVT <= ST ){
+        for (int i = 0; i < qtd_cmp; i++){
+            centers[i].done();
+            centers[i].serving;
+        }
+        GVT++;
+    }
 }
--- src/serv_center.cpp
+++ src/serv_center.cpp
@@ -1,18 +1,11 @@
 #include "../lib/serv_center.hh"
 
-
-
-
 unsigned long GVT;                //global virtual time
-
-
-
 
 
 void component::push_in( agent v ){
 	queue.push(v);
 }
-
 
 void component::serving( void ){
 	for ( int i = 0; i < numb_servers; i++ ){
@@ -28,12 +21,11 @@ void component::serving( void ){
 	}
 }
 
-
 void component::done(void){
 	for( int i = 0; i < numb_servers; i++ ){
 		if(serv[i].use && serv[i].final_time == GVT){
 			serv[i].use = false;
-			/*component[out].push_in(serv[i].user);*/
+			centers[this.out].push_in(serv[i].user);
 		}
 	}
 }
